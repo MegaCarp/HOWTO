d052fe63225580d34925b0f2d752a415f98a777f
MegaCarp <stashkodenis@gmail.com>
2021-06-08 16:24:14 +0300

    init

---
+++ weekly_report.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source_bills=~/Загрузки/Список.mxl.txt 
+all_bills=~/Документы/all_bills
+
+echo "" >> $all_bills
+date >> $all_bills
+
+# TODO for loop per every mng_name
+echo Велинецкий | tee -a $all_bills
+cat $source_bills | grep Велинецкий | wc -l | tee -a $all_bills
+cat $source_bills | grep Велинецкий | sed  's/,.*//g' | sed 's/[^[:alnum:]]\+//g' | paste -sd+ - | bc | tee -a $all_bills
+
+
